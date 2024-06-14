717a59011bd9978d574b8ffccebb8cfaefa87502
estefania <estefania-ets@ieselrincon.es>
2024-06-14 20:30:50 +0200

    Correccion de la incidencia 1

--- Actividad7/src/es/dsw/app/Actividad7Application.java
+++ Actividad7/src/es/dsw/app/Actividad7Application.java
@@ -1,5 +1,7 @@
 package es.dsw.app;
 
+import es.dsw.models.Alumno;
+
 public class Actividad7Application {
 
 	public static void main(String[] args) {
@@ -15,7 +17,8 @@ public class Actividad7Application {
 		Profesor objProfesor = new Profesor("Pepito", "Pérez", "Sanchez", "44332234L");
 		System.out.println("El nuevo profesor se llama " + objProfesor.getNombre());
 
-
+		Alumno objAlumno = new Alumno ("Pepita", "Sánchez", "Martín", "44332234L");
+		System.out.println("El nuevo alumno se llama " + objAlumno.getNombre());
 
 
 		
--- Actividad7/src/es/dsw/models/Alumno.java
+++ Actividad7/src/es/dsw/models/Alumno.java
@@ -8,39 +8,6 @@ public class Alumno extends Profesor {
 		// TODO Auto-generated constructor stub
 	}
 
-	private String nombre, appelido1, apellido2, nif;
-	
 
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getAppelido1() {
-		return appelido1;
-	}
-
-	public void setAppelido1(String appelido1) {
-		this.appelido1 = appelido1;
-	}
-
-	public String getApellido2() {
-		return apellido2;
-	}
-
-	public void setApellido2(String apellido2) {
-		this.apellido2 = apellido2;
-	}
-
-	public String getNif() {
-		return nif;
-	}
-
-	public void setNif(String nif) {
-		this.nif = nif;
-	}
 
 }
